9ae330bf8bd8cf1a3f48b6663eae7e038fa8b180
Alvin Cousins <alvin.cousins@roh.org.uk>
2025-02-07 18:41:46 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react';
+import { getInitialBoard, borderStyle, checkGameState } from './helpers/board';
+import X_image from './assets/X_02.png';
+import O_image from './assets/O_02.png';
+
+import './App.css';
+
+const BOARD_COORDS = ['00', '01', '02', '10', '11', '12', '20', '21', '22'];
+const X = 'X';
+const O = 'O';
+
+const cellImage = (sq) => {
+  if (sq.val === X) {
+    return <img src={X_image} />;
+  }
+  if (sq.val === O) {
+    return <img src={O_image} />;
+  }
+  return null;
+};
+
+function App() {
+  const [winningHighlight, setWinningHighlight] = useState(null);
+  const [board, setBoard] = useState(getInitialBoard(BOARD_COORDS));
+  const [turn, setTurn] = useState(X);
+
+  const handleClick = (clickedSq) => {
+    if (winningHighlight) return;
+    const newBoard = board.map((sq) =>
+      sq.coord === clickedSq.coord ? { ...sq, val: turn } : sq
+    );
+    const winner = checkGameState(newBoard, turn);
+    if (winner) {
+      setWinningHighlight(winner);
+    }
+    setBoard(newBoard);
+    setTurn(turn === X ? O : X);
+  };
+
+  return (
+    <div>
+      <div className='board-wrapper'>
+        <div className='board'>
+          {board.map((sq, i) => {
+            let highlight =
+              winningHighlight && winningHighlight[i] === 1 ? 'highlight' : '';
+
+            return (
+              <span
+                className={`cell-container ${highlight}`}
+                key={sq.coord}
+                style={borderStyle(sq)}
+                onClick={() => handleClick(sq)}
+              >
+                {cellImage(sq)}
+              </span>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/helpers/board.jsx
@@ -0,0 +1,55 @@
+export const getInitialBoard = (coords) =>
+  coords.map((coord) => ({ coord, val: null }));
+
+export const borderStyle = (sq) => {
+  const BORDER_STYLE = '2px solid white';
+  let style = {};
+  if (sq.coord[0] === '0') {
+    style.borderBottom = BORDER_STYLE;
+  }
+  if (sq.coord[0] === '1') {
+    style.borderTop = BORDER_STYLE;
+    style.borderBottom = BORDER_STYLE;
+  }
+  if (sq.coord[0] === '2') {
+    style.borderTop = BORDER_STYLE;
+  }
+  if (sq.coord[1] === '0') {
+    style.borderRight = BORDER_STYLE;
+  }
+  if (sq.coord[1] === '1') {
+    style.borderLeft = BORDER_STYLE;
+    style.borderRight = BORDER_STYLE;
+  }
+  if (sq.coord[1] === '2') {
+    style.borderLeft = BORDER_STYLE;
+  }
+  return style;
+};
+
+const boardsOverlap = (winningBoard, gameBoard) => {
+  let count = 0;
+  for (let i = 0; i < winningBoard.length; i++) {
+    if (winningBoard[i] === 1 && winningBoard[i] === gameBoard[i]) count++;
+    if (count === 3) return true;
+  }
+  return false;
+};
+
+export const checkGameState = (board, mark) => {
+  const winningBoards = [
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1],
+    [1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+  ];
+  const reduced = board.map((sq) => (sq.val === mark ? 1 : 0));
+  const winnerIndex = winningBoards.findIndex((b) => boardsOverlap(b, reduced));
+
+  if (winnerIndex === -1) return null;
+  return winningBoards[winnerIndex];
+};
