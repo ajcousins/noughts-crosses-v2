fe87d015c2365e7d15a1e87bc9830bcef2e499ad
Alvin Cousins <alvin.cousins@roh.org.uk>
2025-02-08 10:32:59 +0000

    Github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/noughts-crosses-v2/',
+  build: {
+    outDir: 'build',
+  },
+  scripts: {
+    predeploy: 'npm run build',
+    deploy: 'gh-pages -d build',
+  },
+});
